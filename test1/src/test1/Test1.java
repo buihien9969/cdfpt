3cf2a9fee378b4c28728f2bbd422688af6e267db
buihien9969 <buihien9969@gmail.com>
2023-05-10 10:29:27 -0700

    finsh-project

---
+++ test1/src/test1/Test1.java
@@ -0,0 +1,111 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package test1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Admin
+ */
+public class Test1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+            // có 2 loại kiểu dũ liệu trong java:
+    // + Nguyên thuỷ: là những kiểu dữ liệu trong C(int, float, double...)
+    // + Object:    int <-> Interger
+    //              double <-> Double
+    //              long <-> Long
+    //              boolean <-> Boolean
+    // ----> có thể tự tạo kiểu dũ liệu Object
+    //=====> Object: + chữ cái đầu luôn luôn viết hoa
+    //               + được khởi tạo bằng từ khoá new
+    //               + khi chấm(.) thì xổ ra gợi ý
+    // Kiểu dữ liệu tên của biến;
+    //VD: int number;
+//    float numberFirst;
+//    String name = "tetstts";
+//    double okiel;
+//    String email = new String();
+    // --> String vừa là kiểu nguyên thuỷ, vừa là kiểu object
+    //1. Tính tổng của 2 số nguyên và in ra màn hình
+    int a = 5;
+    int b = 3;
+    int sum = a + b;
+        System.out.println("sum = "+sum);
+        System.out.println(a + "+" + b + " = " + sum);
+     
+        
+       
+        // 2. Tính hiệu tích thương
+        int hieu = a - b;
+        int tich = a * b;
+        float thuong = a / b;
+        
+        System.out.println("hieu = "+thuong);
+        System.out.println("tich = "+tich);
+        System.out.println("thuong = "+thuong);
+        
+        
+        
+     //3.. Tính chu vi, diện tích hình vuông
+//     int d = 5;
+//     int s = d * d;
+//     int chuvi = 4*d;
+//        System.out.println("s = "+s);
+//        System.out.println("chuvi = "+chuvi);
+//  
+        
+        // NHập xuất thông tin
+        // 1. Nhập thông tin từ bàn phím
+        // trong C: scanf
+        // Trong Java: Scanner
+        //Scanner tenbien khaibao = new....
+//        Scanner sc = new Scanner(System.in);
+        
+        // nhap so nguyen
+//        System.out.println("Nhap vao so nguyen: ");
+//        int number = sc.nextInt();
+//        float ce = sc.nextFloat();
+//        double jsj = sc.nextDouble();
+       // 2. Xuat
+//        System.out.println("So = "+number);
+        
+        
+        // Nhap vao chuoi
+//        String name = sc.nextLine();
+        
+        // next:gặp  dấu cách( ) dừng
+        //nextline: ko dừng khi gặp ()
+        
+        // Bai 4: Nhap ten lop, dia chi, cua ban than tu ban phim va in ra man hinh
+       Scanner lop - 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+
+}
